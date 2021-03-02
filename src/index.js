7ae6571434b444830d64725a4326eee40722d48e
BushraWaheed <72761774+bushrawaheed@users.noreply.github.com>
2021-03-02 16:07:49 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Weather from "./Weather";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>JS Closures</h1>
+      <h2>Challenge </h2>
+      <p>
+        Inside the component, Weather, create a function calculating the
+        temperature in farenheit. Call this function from the JSX being returned
+        in the Weathre component
+      </p>
+      <Weather city="Paris" temperature={19} />
+      <Weather city="New York" temperature={22} />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
